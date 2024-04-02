04008a5cf355a7c799cf9cb772615af1232c962b
alexgriego <agriego20@uan.edu.co>
2024-04-02 10:38:35 -0500

    feat(Auth): 🧭 Actualicé las apis.
    Ahora, django puede hacer que vue reconozca las rutas en el navegador

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +8,5 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
