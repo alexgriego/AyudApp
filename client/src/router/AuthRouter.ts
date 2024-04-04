cd06f785294bf2dfa811464c91ae485e17f40a8b
Jupibel95 <jupibel95@gmail.com>
2024-04-03 20:04:02 -0500

    feat(Aplicación de usuario): ✨ Añadí rutas, Componente y vista de login

---
+++ client/src/router/AuthRouter.ts
@@ -0,0 +1,11 @@
+import LoginView from "@/views/Auth/LoginView.vue";
+
+const AuthRoutes = [
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginView
+    }
+]
+
+export default AuthRoutes
