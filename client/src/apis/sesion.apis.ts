abf52216cc92903406aa2bd03d7e16a54fc11691
Jupibel95 <jupibel95@gmail.com>
2024-04-03 20:00:50 -0500

    feat(Aplicación de usuario): ✨ Añadí tipos. 🛠️ Corregí console.log de apis

--- client/src/apis/sesion.apis.ts
+++ client/src/apis/sesion.apis.ts
@@ -2,6 +2,5 @@ import type { CredentialsTypes } from "@/types/SesionTypes";
 import baseApi from "./base.api";
 
 export const getCredentials = (data: CredentialsTypes) => {
-    console.log(data)
     return baseApi.post('jwt/create', data)
 }
