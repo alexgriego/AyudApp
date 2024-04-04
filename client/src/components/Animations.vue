ef45aea7e3145fb2fe9756256f569e3d1d31b4c3
Jupibel95 <jupibel95@gmail.com>
2024-04-03 20:03:31 -0500

    feat(Aplicación de usuario): ✨ Añadí layout y Animaciones

---
+++ client/src/components/Animations.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue"
+import { LottieAnimation } from "lottie-web-vue"
+const props = defineProps<{
+    ICON: object;
+}>();
+
+let anim = ref()
+
+onMounted(() => {
+    setTimeout(() => {
+        console.log(anim.value.goToAndPlay(150, true))
+        anim.value
+    }, 500)
+})
+</script>
+<template>
+    <LottieAnimation :animation-data="props.ICON" :auto-play="true" :loop="true" :speed="1" ref="anim" />
+</template>
---
+++ client/src/components/CardLayout.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="col col-lg-6">
+        <div class="card card-default">
+            <div class="card-header">
+                <h5>{{ props.header }}</h5>
+                <div class="card-tools" v-for="item in props?.buttons" v-if="props?.buttons">
+                    <RouterLink :to="item.link" :class="item.class" v-if="item.type !== 'button'">
+                        <i :class="item.icon"> </i>
+                        {{ item.text }}
+                    </RouterLink>
+                    <button :class="item.class" v-else>
+                        <i :class="item.icon"></i>
+                        {{ item.text }}
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">{{ props.title }}</h5>
+                <p class="card-text mt-5">
+                    <!--Aquí se importa así-->
+                    <component :is="props.component" />
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+    header: string;
+    title: string;
+
+    /*animation: object ;*/
+    buttons: Array<{
+        text: string;
+        link: string;
+        icon: string;
+        class: string;
+        type: string;
+    }> | null;
+
+    component: Object; //Componente aquí
+}>();
+</script>
