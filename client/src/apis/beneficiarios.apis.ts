761bfd8bba067060d39bef98d1401eee40f30dc9
alexgriego <agriego20@uan.edu.co>
2024-04-06 16:33:44 -0500

    feat(Aplicación de personas): 📡Añadí APIS de beneficiarios

---
+++ client/src/apis/beneficiarios.apis.ts
@@ -0,0 +1,47 @@
+import baseApi from './base.api';
+import SesionStore from '@/stores/SesionStore';
+
+export const getBenefinciarios = async () => {
+    const sesion = SesionStore();
+    return baseApi.get('personas/beneficiarios/', {
+        headers: {
+            Authorization: `JWT ${sesion.PAT}`
+        }
+    });
+}
+
+export const getBeneficiario = async (id: number) => {
+    const sesion = SesionStore();
+    return baseApi.get(`personas/beneficiarios/${id}/`, {
+        headers: {
+            Authorization: `JWT ${sesion.PAT}`
+        }
+    });
+}
+
+export const saveBeneficiario = async (data: any) => {
+    const sesion = SesionStore();
+    return baseApi.post('personas/beneficiarios/', data, {
+        headers: {
+            Authorization: `JWT ${sesion.PAT}`
+        }
+    });
+}
+
+export const updateBeneficiario = async (id: number, data: any) => {
+    const sesion = SesionStore();
+    return baseApi.put(`personas/beneficiarios/${id}/`, data, {
+        headers: {
+            Authorization: `JWT ${sesion.PAT}`
+        }
+    });
+}
+
+export const deleteBeneficiario = async (id: number) => {
+    const sesion = SesionStore();
+    return baseApi.delete(`personas/beneficiarios/${id}/`, {
+        headers: {
+            Authorization: `JWT ${sesion.PAT}`
+        }
+    });
+}
