c5feb43c5b5d3eb1dc82628ddbe80df8301f038a
Jupibel95 <jupibel95@gmail.com>
2024-04-05 20:22:38 -0500

    feat(Aplicación de usuario): ⚰Eliminé codigo no innecesario

--- client/src/types/SesionTypes.ts
+++ client/src/types/SesionTypes.ts
@@ -1,11 +1,3 @@
-export interface UserTypes {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-    groups: Array<{ id: string, name: string }>;
-    status: string;
-}
 
 export interface CredentialsTypes {
     username: string;
