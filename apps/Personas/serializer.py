15e6db0c9585d0e31e8669d47148d92582133144
alexgriego <alexgriego20@uan.edu.co>
2024-04-20 17:20:51 -0500

    feat (Aplicación de personas): ✨ Añadí CRUD de Patrocinadores

--- apps/Personas/serializer.py
+++ apps/Personas/serializer.py
@@ -1,8 +1,14 @@
-from .models import Beneficiario
+from .models import Beneficiario, Patrocinador
 from rest_framework import serializers
 
 
 class BeneficiarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Beneficiario
+        fields = '__all__'
+
+
+class PatrocinadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patrocinador
         fields = '__all__'
--- apps/Personas/views.py
+++ apps/Personas/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from core.permissions import admin_or_encuenstador_required
-from .serializer import BeneficiarioSerializer
-from .models import Beneficiario
+from .serializer import BeneficiarioSerializer, PatrocinadorSerializer
+from .models import Beneficiario, Patrocinador
 
 
 class BeneficiarioAPI(APIView):
@@ -20,7 +20,6 @@ class BeneficiarioAPI(APIView):
     def get(self, request, pk=None, format=None):
         queryset = self.get_beneficiario(
             pk) if pk else self.get_beneficiarios()
-        print(queryset)
         serializer = BeneficiarioSerializer(
             queryset, many=True)
         return Response(serializer.data)
@@ -47,4 +46,47 @@ class BeneficiarioAPI(APIView):
         beneficiario = self.get_beneficiario(pk)
         beneficiario.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    # Create your views here.
+
+
+class PatrocinadoresAPI(APIView):
+    def get_patrocinadores(self):
+        return Patrocinador.objects.order_by('-created_at')[:10]
+
+    def get_patrocinador(self, pk):
+        return Patrocinador.objects.filter(NIT__istartswith=pk)
+
+    def find_patrocinador(self, pk):
+        return Patrocinador.objects.get(NIT=pk)
+
+    @admin_or_encuenstador_required
+    def get(self, request, pk=None, format=None):
+        queryset = self.get_patrocinador(
+            pk) if pk else self.get_patrocinadores()
+        print(queryset)
+        serializer = PatrocinadorSerializer(
+            queryset, many=True)
+        return Response(serializer.data)
+
+    @admin_or_encuenstador_required
+    def post(self, request, format=None):
+        serializer = PatrocinadorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @admin_or_encuenstador_required
+    def put(self, request, pk, format=None):
+        patrocinador = self.find_patrocinador(pk)
+        serializer = PatrocinadorSerializer(patrocinador, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @admin_or_encuenstador_required
+    def delete(self, request, pk, format=None):
+        patrocinador = self.get_patrocinador(pk)
+        patrocinador.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
