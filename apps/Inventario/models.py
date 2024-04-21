7ec18b197a47a04a1a2177c845505b3307de9b67
alexgriego <alexgriego20@uan.edu.co>
2024-04-21 16:51:59 -0500

    feat (Aplicación de ayudas): ✨Añadí CRUD de ayudas económicas

--- apps/Inventario/models.py
+++ apps/Inventario/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from apps.Personas.models import Patrocinador
 
 
 class Producto(models.Model):
@@ -6,7 +7,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(null=True, blank=True)
     cantidad = models.IntegerField(default=1)
-    tipo = models.CharField(max_length=100,default='Alimento')
+    tipo = models.CharField(max_length=100, default='Alimento')
     es_perecedero = models.BooleanField(default=False)
     fecha_vencimiento = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,3 +15,50 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+class Dinero(models.Model):
+    patrocinador = models.ForeignKey(
+        Patrocinador, on_delete=models.SET_NULL, null=True, blank=True)
+    cantidad = models.DecimalField(max_digits=100, decimal_places=2)
+    fecha = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=100, default='admin')
+
+    def __str__(self):
+        return f'{self.cantidad} - {self.patrocinador}'
+
+
+class JornadaAyuda(models.Model):
+    fecha = models.DateField(auto_now=True)
+    descripcion = models.TextField(null=True, blank=True)
+    nombre = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=100, default='admin')
+
+    def __str__(self):
+        return f'{self.nombre}'
+
+
+class DineroJornada(models.Model):
+    jornada = models.ForeignKey(JornadaAyuda, on_delete=models.CASCADE)
+    dinero = models.ForeignKey(Dinero,
+                               on_delete=models.SET_NULL, null=True, blank=True, unique=True)
+    cantidad = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=100, default='admin')
+
+    def __str__(self):
+        return f'{self.jornada} - {self.dinero.patrocinador.nombre}'
+
+
+class ProductoJornada(models.Model):
+    jornada = models.ForeignKey(JornadaAyuda, on_delete=models.CASCADE)
+    producto = models.ForeignKey(
+        Producto, on_delete=models.SET_NULL, null=True, blank=True)
+    cantidad = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=100, default='admin')
+
+    def __str__(self):
+        return f'{self.jornada} - {self.producto.nombre}'
--- apps/Inventario/views.py
+++ apps/Inventario/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from apps.Personas.models import Patrocinador
 from core.permissions import admin_or_encuenstador_required
-from .models import Producto
-from .serializers import ProductoSerializer
+from .models import Producto, Dinero
+from .serializers import ProductoSerializer, DineroSerializer
 from django.db.models import Q
+
+# APIS de inventario
 
 
 class ProductoAPI(APIView):
@@ -30,7 +33,7 @@ class ProductoAPI(APIView):
         serializer = ProductoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @ admin_or_encuenstador_required
@@ -46,4 +49,54 @@ class ProductoAPI(APIView):
     def delete(self, request, pk, format=None):
         producto = self.get_producto(pk)
         producto.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# APIS de Ayuda economica
+class DineroAPI(APIView):
+    def get_dineros(self):
+        return Dinero.objects.order_by('-created_at')[:10]
+
+    def get_dinero(self, pk):
+        return Dinero.objects.filter(Q(id=pk) | Q(patrocinador__nombre__icontains=pk)).order_by('-created_at')
+
+    def find_dinero(self, pk):
+        return Dinero.objects.get(id=pk)
+
+    @ admin_or_encuenstador_required
+    def get(self, request, pk=None, format=None):
+        queryset = self.get_dinero(
+            pk) if pk else self.get_dineros()
+        serializer = DineroSerializer(
+            queryset, many=True)
+        return Response(serializer.data)
+
+    @ admin_or_encuenstador_required
+    def post(self, request, format=None):
+        q = Dinero(cantidad=request.data['cantidad'], patrocinador=Patrocinador.objects.get(
+            pk=request.data['patrocinador']) if request.data['patrocinador'] else None)
+        try:
+            q.save()
+            return Response(status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    @ admin_or_encuenstador_required
+    def put(self, request, pk, format=None):
+        dinero = self.find_dinero(pk)
+        print(request.data['patrocinador'])
+        try:
+            dinero.patrocinador = Patrocinador.objects.get(
+                pk=request.data['patrocinador']) if request.data['patrocinador'] else None
+            dinero.cantidad = request.data['cantidad']
+            dinero.save()
+            return Response(status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    @ admin_or_encuenstador_required
+    def delete(self, request, pk, format=None):
+        dinero = self.find_dinero(pk)
+        dinero.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
