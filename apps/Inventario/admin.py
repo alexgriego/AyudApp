2915184422e2a7fe40fa076d5d0cffbcf9474bb8
alexgriego <agriego20@uan.edu.co>
2024-04-23 00:36:07 -0500

    feat (Aplicación de ayudas): ✨Añadi CRUD de jornadas y corregí errores

--- apps/Inventario/admin.py
+++ apps/Inventario/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Producto, Dinero, JornadaAyuda, DineroJornada, ProductoJornada
+from .models import Producto, Dinero, JornadaAyuda
 # Register your models here.
 admin.site.register(Producto)
 admin.site.register(Dinero)
 admin.site.register(JornadaAyuda)
-admin.site.register(DineroJornada)
-admin.site.register(ProductoJornada)
