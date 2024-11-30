ef370c63bcd78d8cfbf2b230047827bf6d1f94b1
Jupibel95 <71595087+alexgriego@users.noreply.github.com>
2024-11-30 10:34:29 -0500

    feat: test

--- apps/Personas/views.py
+++ apps/Personas/views.py
@@ -110,4 +110,3 @@ class DetalleBeneficiarioAPI(APIView):
 class PatrocinadorSearch(APIView):
     def get_patrocinador(self, pk):
         return Patrocinador.objects.get(Q(NIT=pk) | Q(nombre=pk))
-
